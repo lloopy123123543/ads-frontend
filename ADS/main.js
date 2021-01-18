e009c155a9eb9fe533bd2436fb42c84ea9af545d
lloopy123123543 <lloopy123123543@gmail.com>
2021-01-18 19:45:28 +0300

    lllll

--- ADS/main.js
+++ ADS/main.js
@@ -1,3 +1,5 @@
+
+
 $('.Header__button').click(function() { 
 	
 	$('.Vmodel').fadeIn();
@@ -8,3 +10,36 @@ $('.Modal__close').click(function() {
 	$('.Vmodel').fadeOut();
 	
 });
+
+$('.Model__bot__button').click(function(){
+	$('.Vmodel').fadeOut();
+})
+
+$('.Model__bot__button').click(function(){
+	$('.Alert').fadeIn();
+	$('.Alert').addClass('anabled')
+	$('.Vmodel').addClass('disabled');
+}) 
+
+$(window).on('load', function () { 
+	setTimeout(function(){
+		if($('.Vmodel').hasClass('disabled')) {
+			return false;
+		} else {
+			
+			$(".Vmodel").fadeOut();
+		}
+	}, );
+});
+
+$(window).on('load', function () { 
+	setTimeout(function(){
+		if($('.Alert').hasClass('disabled')) {
+			return false;
+		} else {
+			
+			$(".Alert").fadeOut();
+		}
+	}, );
+});
+
