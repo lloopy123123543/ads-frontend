be4a4597f90e12740df442a1f372ed5b0d647784
lloopy123123543 <lloopy123123543@gmail.com>
2021-01-25 16:16:55 +0300

    lll

--- ADS/main.js
+++ ADS/main.js
@@ -1,14 +1,14 @@
 
 
-$('.Header__button').click(function() { 
-	
+$('.Header__button').click(function() {
+
 	$('.Vmodel').fadeIn();
 	$('.Vmodel').addClass('disabled');
 });
 
-$('.Modal__close').click(function() { 
+$('.Modal__close').click(function() {
 	$('.Vmodel').fadeOut();
-	
+
 });
 
 $('.Model__bot__button').click(function(){
@@ -20,34 +20,100 @@ $('.Model__bot__button').click(function(){
 	$('.Alert').addClass('anabled')
 	$('.Vmodel').addClass('disabled');
 	console.log(1);
-}) 
+})
 
-$(window).on('load', function () { 
+$(window).on('load', function () {
 	setTimeout(function(){
 		if($('.Vmodel').hasClass('disabled')) {
 			return false;
 		} else {
-			
+
 			$(".Vmodel").fadeOut(1);
 		}
 	}, );
 });
 
-$(window).on('load', function () { 
+$(window).on('load', function () {
 	setTimeout(function(){
 		if($('.Alert').hasClass('disabled')) {
 			return false;
 		} else {
-			
+
 			$(".Alert").fadeOut(1);
 		}
 	}, );
 
 });
-$('.Modal__close1').click(function() { 
+$('.Modal__close1').click(function() {
 	$('.Alert').fadeOut();
-		
+
 	});
+
+
+
 $(window).on('load', function() {
 	$('.preloader').fadeOut(1500);
 	});
+
+
+
+
+
+	$(document).ready(function () {
+	    var result = '';
+	    $('input').change(function () {
+	        if (result == '') {
+	            result += $(this).val();
+	        }
+	        else {
+	            result += ', ' + $(this).val();
+	        }
+
+	        $('#divResult').html(result);
+	    });
+	});
+
+	$(document).ready(function () {
+    var result = '';
+    $('.inputRequiredName').change(function () {
+        if (result == '') {
+            result += $(this).val();
+        }
+        else {
+            result += ', ' + $(this).val();
+        }
+
+        $('#divResultName').html(result);
+    });
+});
+
+
+$(document).ready(function () {
+	var result = '';
+	$('.inputRequiredNumber').change(function () {
+			if (result == '') {
+					result += $(this).val();
+			}
+			else {
+					result += ', ' + $(this).val();
+			}
+
+			$('#divResultNumber').html(result);
+	});
+});
+
+
+
+$(document).ready(function () {
+	var result = '';
+	$('.inputRequiredText').change(function () {
+			if (result == '') {
+					result += $(this).val();
+			}
+			else {
+					result += '\n ' + $(this).val();
+			}
+
+			$('#divResultText').html(result);
+	});
+});
