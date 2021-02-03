50e140f82d672439e7798ce5120c0bc6d082c094
lloopy123123543 <lloopy123123543@gmail.com>
2021-02-03 11:01:25 +0300

    lll

--- ADS/main.js
+++ ADS/main.js
@@ -1,125 +1,128 @@
+$(document).ready(function() {
 
-$('.Content__box').ready(function(){
-	$('.Content__box').fadeOut();
-})
+    // Основные переменные приложения.
+    var arrayAds = Array(); // Массив объявлений.
 
-
-$('.Header__button').click(function() {
-
-	$('.Vmodel').fadeIn();
-	$('.Vmodel').addClass('disabled');
-});
-
-$('.Modal__close').click(function() {
-	$('.Vmodel').fadeOut();
-
-});
-
-$('.Model__bot__button').click(function(){
-	$('.Vmodel').fadeOut();
-})
-
-$('.Model__bot__button').click(function(){
-	$('.Content__box').fadeIn();
-	$('.Alert').fadeIn();
-	$('.Alert').addClass('anabled')
-	$('.Vmodel').addClass('disabled');
-	var chack = 1+1;
-console.log(++chack);
-})
-
-$(window).on('load', function () {
-	setTimeout(function(){
-		if($('.Vmodel').hasClass('disabled')) {
-			return false;
-		} else {
-
-			$(".Vmodel").fadeOut(1);
-		}
-	}, );
-});
-
-$(window).on('load', function () {
-	setTimeout(function(){
-		if($('.Alert').hasClass('disabled')) {
-			return false;
-		} else {
-
-			$(".Alert").fadeOut(1);
-		}
-	}, );
-
-});
-$('.Modal__close1').click(function() {
-	$('.Alert').fadeOut();
-
-	});
-
-
-
-$(window).on('load', function() {
-	$('.preloader').fadeOut(1500);
-	});
-
-
-
-
-
-	$(document).ready(function () {
-	    var result = '';
-	    $('input').change(function () {
-	        if (result == '') {
-	            result += $(this).val();
-	        }
-	        else {
-	            result += ', ' + $(this).val();
-	        }
-
-	        $('#divResult').html(result);
-	    });
-	});
-
-	$(document).ready(function () {
-    var result = '';
-    $('.inputRequiredName').change(function () {
-        if (result == '') {
-            result += $(this).val();
-        }
-        else {
-            result += ', ' + $(this).val();
-        }
-
-        $('#divResultName').html(result);
+    $('.Header__button').click(function() {
+        $('.Vmodal').show();
     });
+
+    $('#close').click(function() {
+        $('#modal').hide();
+    });
+
+
+    // Добавляем новое обявление.
+    $('#btnSubmit').click(function() {
+        var ad = {
+            text: $('textarea[name="text"]').val(),
+            contactName: $('input[name="name"]').val(),
+            contactPhone: $('input[name="phone"]').val()
+        }
+
+        arrayAds.push(ad);
+
+        console.log(arrayAds);
+
+        // Отрисовка новых объявлений.
+        renderAds();
+
+        // Скрыть модальное окно.
+        $('.Vmodel').hide();
+        $('textarea[name="text"]').val('');
+        $('input[name="name"]').val('');
+        $('input[name="phone"]').val('');
+    });
+
+
+
+
+    // Функция отрисовки новых объявлений.
+    function renderAds() {
+
+        // Очищаем блок контента.
+        $('#content').html('');
+
+        // Отрисовываем новые объявления.
+        arrayAds.map(ad => {
+            var adHTML =
+            "<div class='Content__box'>" +
+                "<div class='Box__info'>" + ad.text + "</div>" +
+                "<div class='Box__name'>" + ad.contactName + "</div>" +
+                "<div class='Box__phone'>" + ad.contactPhone + "</div>" +
+            "</div>";
+
+            $('#content').append(adHTML);
+        });
+    }
+
 });
 
 
-$(document).ready(function () {
-	var result = '';
-	$('.inputRequiredNumber').change(function () {
-			if (result == '') {
-					result += $(this).val();
-			}
-			else {
-					result += ', ' + $(this).val();
-			}
-
-			$('#divResultNumber').html(result);
-	});
-});
-
-
-
-$(document).ready(function () {
-	var result = '';
-	$('.inputRequiredText').change(function () {
-			if (result == '') {
-					result += $(this).val();
-			}
-			else {
-					result += '\n ' + $(this).val();
-			}
-
-			$('#divResultText').html(result);
-	});
-});
+// $('.Content__box').ready(function(){
+// 	$('.Content__box').fadeOut();
+// })
+//
+//
+// $('.Header__button').click(function() {
+//
+// 	$('.Vmodel').fadeIn();
+// 	$('.Vmodel').addClass('disabled');
+// });
+//
+// $('.Modal__close').click(function() {
+// 	$('.Vmodel').fadeOut();
+//
+// });
+//
+// $('.Model__bot__button').click(function(){
+// 	$('.Content__box').fadeIn();
+// 	$('.Alert').fadeIn();
+// 	$('.Alert').addClass('anabled')
+// 	$('.Vmodel').addClass('disabled');
+// })
+//
+// $(window).on('load', function () {
+// 	setTimeout(function(){
+// 		if($('.Vmodel').hasClass('disabled')) {
+// 			return false;
+// 		} else {
+//
+// 			$(".Vmodel").fadeOut(1);
+// 		}
+// 	}, );
+// });
+//
+// $(window).on('load', function () {
+// 	setTimeout(function(){
+// 		if($('.Alert').hasClass('disabled')) {
+// 			return false;
+// 		} else {
+//
+// 			$(".Alert").fadeOut(1);
+// 		}
+// 	}, );
+//
+// });
+// $('.Modal__close1').click(function() {
+// 	$('.Alert').fadeOut();
+//
+// 	});
+//
+// $('#new').click(function(){
+// 	var ad = {
+// 		text = $('input [name="TA"]').val(),
+// 		contactName = $('input [name="NA"]').val(),
+// 		contactNumber = $('input [name="NUA"]').val(),
+//
+// 	}
+// 	arrayAds.push(ad);
+// 	console.log(arreyAds);
+//
+// 	$('.Vmodel').hide();
+// });
+//
+// function renderAds() {
+// 	var adHTML =
+// 	""
+// }
